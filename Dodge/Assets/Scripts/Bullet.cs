9baf44cc1fe27582f87a01c0dd61fa7e5eaab0d9
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-02-04 22:14:38 +0900

    2023.02.04

--- Dodge/Assets/Scripts/Bullet.cs
+++ Dodge/Assets/Scripts/Bullet.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+
     void Start()
     {
-        
+        bulletRigidbody = GetComponent<Rigidbody>(); // Rigidbody 컴포넌트를 가져옴
+        bulletRigidbody.velocity = transform.forward * speed; // 초당 speed만큼 앞쪽으로 이동
+
+        Destroy(gameObject, 3f); // 3초 뒤에 자신의 게임오브젝트 파괴
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if(playerController != null) 
+            {
+                playerController.Die();
+            }
+        }
     }
 }
