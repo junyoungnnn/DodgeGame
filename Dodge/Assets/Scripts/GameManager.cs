ef7540bbd5315efdf53a5b8ae39b911b63867ba5
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-02-06 01:10:11 +0900

    2024.02.05

--- Dodge/Assets/Scripts/GameManager.cs
+++ Dodge/Assets/Scripts/GameManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameover;
+
     void Start()
     {
-        
+        surviveTime = 0;
+        isGameover = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time: " + (int)surviveTime;
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameover = true;
+        gameoverText.SetActive(true);
     }
 }
